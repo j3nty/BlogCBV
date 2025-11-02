1ef674fdc7c5c7a0345b6d54d5a151aca0533f28
j3nty <j3nty.mail@gmail.com>
2025-11-02 15:23:40 +0200

    юрлки для списка постов

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import HomePageView
+from app.views import HomePageView, PostListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
+    path("posts/", PostListView.as_view(), name="post_list"),
 ]
