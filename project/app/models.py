1f700395b1ab0e2322e533f31fe69c60b6774d7c
j3nty <j3nty.mail@gmail.com>
2025-11-02 10:38:47 +0200

    First commit

---
+++ project/app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name="user",
+        on_delete=models.CASCADE,
+    )
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey(
+        AuthorProfile,
+        related_name="author",
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name="category",
+        on_delete=models.CASCADE,
+    )
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
