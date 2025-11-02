a13ee4f3f018a7002d00e8a071a9b0c455d73236
j3nty <j3nty.mail@gmail.com>
2025-11-02 15:21:50 +0200

    Админка

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import BlogPost, AuthorProfile, Category
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "content",
+        "author",
+        "category",
+        "published",
+    ]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(AuthorProfile)
+class AuthorProfileAdmin(admin.ModelAdmin):
+    list_display = ["user"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
