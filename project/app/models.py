84f6e1de4da7ebcaf7e33bb5b80a81bd1ff3c154
j3nty <j3nty.mail@gmail.com>
2025-11-02 11:03:39 +0200

    save для слага

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -33,6 +34,11 @@ class BlogPost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargss)
 
     def get_absolute_url(self):
         return reverse("blog", kwargs={"pk": self.pk, "slug": self.slug})
