c6f7fc2a47f7c8269a5ca3538e33b02bec4e2cec
j3nty <j3nty.mail@gmail.com>
2025-11-02 11:23:18 +0200

    TemplateView

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from datetime import datetime
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        hour = datetime.now().hour
+        if 0 <= hour < 6:
+            greeting = "Доброй ночи"
+        elif 6 <= hour < 12:
+            greeting = "Доброе утро"
+        elif 12 <= hour < 18:
+            greeting = "Добрый день"
+        else:
+            greeting = "Добрый вечер"
+
+        context["current_time"] = greeting
+        return context
